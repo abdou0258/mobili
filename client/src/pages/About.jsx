d1b49b96e406a87724fab3b66cea7f38a09fe2c6
abdou0258 <123670398+abdou0258@users.noreply.github.com>
2024-06-20 15:06:06 +0100

    finall

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -15,10 +15,10 @@ function About() {
         <h1 className='font-bold'>Mobili ?</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent felis orci, porta et velit in, lobortis molestie magna. <br /> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed accumsan, ex quis auctor pellentesque, velit dolor volutpat elit, a elementum lectus neque vestibulum libero. <br /> Mauris finibus nisl libero, ac pretium orci sollicitudin ut. Nullam vel aliquam est, et lobortis ligula. Vivamus vel ex efficitur, tempus urna aliquet, hendrerit nibh. Nulla laoreet lorem ut ante hendrerit condimentum. Duis id egestas ligula, nec vulputate elit.</p>
       </div>
-      <main className='flex justify-center items-center w-2/3 mx-auto gap-20 mt-20'>
+      <main className='flex flex-col md:flex-row  justify-center items-center w-2/3 mx-auto gap-20 mt-20'>
 
       <h1 className='font-bold'>Discover <br /> your ideal<br /> proprety</h1>
-      <div className='border-l-2 ps-10'>
+      <div className='md:border-l-2 ps-10'>
         <h5 >Finding the perfect place to call home can be hard. Our team <br />of creatives and specialists makes real estate easy.</h5>
         <Link to={"/search?"}>
             <button
